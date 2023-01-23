9395781aba0cf1d1fa6ec61c7437fc101bcf34e1
danieljaime17 <danijaime17@gmail.com>
2023-01-23 11:03:53 +0100

    This version is a test to proof that the ESP32 device is able to connect to internet

--- Main_Program/Main_Program.ino
+++ Main_Program/Main_Program.ino
@@ -1,23 +1,63 @@
-
+#include "CTBot.h"
 #include <PZEM004Tv30.h>
+#include "token.h"
+//----------------------------InitSensor--------------------------------------
 
 #if defined(ESP32)
 PZEM004Tv30 pzem(Serial2, 16, 17);
 #else
 PZEM004Tv30 pzem(Serial2);
 #endif
+//---------------------------Init_TelegramBot------------------------------------------
+template<class T> inline Print &operator <<(Print &obj, T arg) {
+  obj.print(arg);
+  return obj;
+}
+
+
+CTBot miBot;
+
+
+
 
 void setup() {
     Serial.begin(115200);
+    Serial.println("Iniciando Bot de Telegram");
+  
+    miBot.wifiConnect(ssid, password);
+
+    miBot.setTelegramToken(token);
+
+    if (miBot.testConnection()) {
+    Serial.println("\n Conectado");
+  }
+  else {
+    Serial.println("\n Problemas Auxilio");
+  }
+
 
     // to reset the internal energy counter pzem.resetEnergy()
 }
 
+
+
+
 void loop() {
-        
+
+    TBMessage msg;
+
+    if (CTBotMessageText == miBot.getNewMessage(msg)) {
+    Serial << "Mensaje: " << msg.sender.firstName << " - " <<  msg.text << "\n";
+    miBot.sendMessage(msg.sender.id, msg.text);
+  }
+
+  delay(500);
+    
+    /*
     Serial.print("Custom Address:");
     Serial.println(pzem.readAddress(), HEX);
 
+    
     // Read the data from the sensor
     float voltage = pzem.voltage();
     float current = pzem.current();
@@ -42,14 +82,15 @@ void loop() {
     } else {
 
         // Print the values to the Serial console
-        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
-        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
-        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
-        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
-        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
-        Serial.print("PF: ");           Serial.println(pf);
+        //Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+        //Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+        //Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+        //Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+        //Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+        //Serial.print("PF: ");           Serial.println(pf);
 
     }
+    */
 
     Serial.println();
     delay(2000);
