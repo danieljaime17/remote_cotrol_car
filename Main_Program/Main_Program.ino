e45ca7cf6daba0aab8fbf4f132e505bc17bad4ac
danieljaime17 <danijaime17@gmail.com>
2023-01-23 21:22:47 +0100

    The code is working and is able to send messages

--- Main_Program/Main_Program.ino
+++ Main_Program/Main_Program.ino
@@ -1,97 +1,53 @@
-#include "CTBot.h"
-#include <PZEM004Tv30.h>
-#include "token.h"
-//----------------------------InitSensor--------------------------------------
-
-#if defined(ESP32)
-PZEM004Tv30 pzem(Serial2, 16, 17);
-#else
-PZEM004Tv30 pzem(Serial2);
-#endif
-//---------------------------Init_TelegramBot------------------------------------------
 template<class T> inline Print &operator <<(Print &obj, T arg) {
   obj.print(arg);
   return obj;
 }
 
-
+#include "CTBot.h"
 CTBot miBot;
+int Led = 2;
 
-
-
+#include "token.h"
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("Iniciando Bot de Telegram");
-  
-    miBot.wifiConnect(ssid, password);
+  Serial.begin(115200);
+  Serial.println("Iniciando Bot de Telegram");
 
-    miBot.setTelegramToken(token);
+  pinMode(Led, OUTPUT);
+  digitalWrite(Led, HIGH);
 
-    if (miBot.testConnection()) {
+  miBot.wifiConnect(ssid, password);
+
+  miBot.setTelegramToken(token);
+
+  if (miBot.testConnection()) {
     Serial.println("\n Conectado");
   }
   else {
     Serial.println("\n Problemas Auxilio");
   }
-
-
-    // to reset the internal energy counter pzem.resetEnergy()
 }
 
-
-
-
 void loop() {
+  TBMessage msg;
 
-    TBMessage msg;
-
-    if (CTBotMessageText == miBot.getNewMessage(msg)) {
+  if (CTBotMessageText == miBot.getNewMessage(msg)) {
     Serial << "Mensaje: " << msg.sender.firstName << " - " <<  msg.text << "\n";
-    miBot.sendMessage(msg.sender.id, msg.text);
+
+    if (msg.text.equalsIgnoreCase("encender")) {
+      Serial.println("Encender Led");
+      digitalWrite(Led, HIGH);
+      miBot.sendMessage(msg.sender.id, "Led Enendido");
+    }
+    else if (msg.text.equalsIgnoreCase("apagar")) {
+      Serial.println("Apagar Led");
+      digitalWrite(Led, LOW);
+      miBot.sendMessage(msg.sender.id, "Led Apagado");
+    }
+    else {
+     miBot.sendMessage(msg.sender.id, "Bienbenido " + msg.sender.firstName + ",intenta usar: encender o apagar");
+    }
   }
 
-  delay(500);
-    
-    /*
-    Serial.print("Custom Address:");
-    Serial.println(pzem.readAddress(), HEX);
-
-    
-    // Read the data from the sensor
-    float voltage = pzem.voltage();
-    float current = pzem.current();
-    float power = pzem.power();
-    float energy = pzem.energy();
-    float frequency = pzem.frequency();
-    float pf = pzem.pf();
-
-    // Check if the data is valid
-    if(isnan(voltage)){
-        Serial.println("Error reading voltage");
-    } else if (isnan(current)) {
-        Serial.println("Error reading current");
-    } else if (isnan(power)) {
-        Serial.println("Error reading power");
-    } else if (isnan(energy)) {
-        Serial.println("Error reading energy");
-    } else if (isnan(frequency)) {
-        Serial.println("Error reading frequency");
-    } else if (isnan(pf)) {
-        Serial.println("Error reading power factor");
-    } else {
-
-        // Print the values to the Serial console
-        //Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
-        //Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
-        //Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
-        //Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
-        //Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
-        //Serial.print("PF: ");           Serial.println(pf);
-
-    }
-    */
-
-    Serial.println();
-    delay(2000);
+  delay(250);
 }
